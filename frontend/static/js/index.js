31a76f666c7f97ce6ba2aba8db12d93d51831930
enkoding1 <ksjdevelop53@gmail.com>
2022-08-03 23:41:04 +0900

    update

---
+++ frontend/static/js/index.js
@@ -0,0 +1,8 @@
+const router = async () =>{
+    const routes = [
+        {path:'/', view:()=>{console.log('view dashboard')}},
+        {path:'/posts', view:()=>{console.log('view posts')}},
+        {path:'/settings', view:()=>{console.log('view settings')}}
+    
+    ]
+}
